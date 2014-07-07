b38e9f7ee959c5b41abc14b12f02fc24ccdb7df6
Falcon760 <sfalcon266@hotmail.com>
2014-07-07 17:00:03 -0400

    Changes1

--- ContosoUniversity/DAL/StudentContext.cs
+++ ContosoUniversity/DAL/StudentContext.cs
@@ -12,34 +12,21 @@ namespace ContosoUniversity.DAL
     public class SchoolContext : DbContext
     {
 
-
-
-        public SchoolContext()
-            : base("SchoolContext")
+        //constructor with the method SchoolContext() giving the db name SchoolContext
+        public SchoolContext() : base("SchoolContext")
         {
 
         }
-
-
-
-
-
         public DbSet<Student> Students { get; set; }
-
         public DbSet<Enrollment> Enrollments { get; set; }
-
         public DbSet<Course> Courses { get; set; }
-
-
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
-
-
-
             modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
-
         }
 
     }
 }
+
+//this file uses DbSet to set the tables with the corresponding info from the classes
--- ContosoUniversity/Models/Enrollment.cs
+++ ContosoUniversity/Models/Enrollment.cs
@@ -3,32 +3,21 @@
 
     public enum Grade
     {
-
         A, B, C, D, F
-
     }
-
-
-
-
 
     public class Enrollment
     {
 
         public int EnrollmentID { get; set; }
-
         public int CourseID { get; set; }
-
         public int StudentID { get; set; }
-
         public Grade? Grade { get; set; }
-
-
-
         public virtual Course Course { get; set; }
-
         public virtual Student Student { get; set; }
-
     }
 
 }
+
+//foreign key property
+//<navigation property name><primary key property name>
