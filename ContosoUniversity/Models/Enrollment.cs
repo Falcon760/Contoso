492e7e268150f8342dcc7141061f0bf09ae14f1b
Falcon760 <sfalcon266@hotmail.com>
2014-07-10 15:49:44 -0400

    ThursC

--- ContosoUniversity/Models/Enrollment.cs
+++ ContosoUniversity/Models/Enrollment.cs
@@ -1,4 +1,7 @@
-﻿namespace ContosoUniversity.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace ContosoUniversity.Models
 {
 
     public enum Grade
@@ -12,6 +15,7 @@
         public int EnrollmentID { get; set; }
         public int CourseID { get; set; }
         public int StudentID { get; set; }
+        [DisplayFormat(NullDisplayText="No Grade")]
         public Grade? Grade { get; set; }
         public virtual Course Course { get; set; }
         public virtual Student Student { get; set; }
