9d53fcc65fd6f5596a2a04fe66a72d4b25caf19e
Falcon760 <sfalcon266@hotmail.com>
2014-07-10 19:54:33 -0400

    sdasd

--- ContosoUniversity/DAL/StudentContext.cs
+++ ContosoUniversity/DAL/StudentContext.cs
@@ -1,32 +1,24 @@
-﻿using ContosoUniversity.Models; 
-
-using System.Data.Entity; 
-
+﻿using ContosoUniversity.Models;
+using System.Data.Entity;
 using System.Data.Entity.ModelConfiguration.Conventions;
-
-
-
 namespace ContosoUniversity.DAL
 {
-
     public class SchoolContext : DbContext
     {
-
-        //constructor with the method SchoolContext() giving the db name SchoolContext
-        public SchoolContext() : base("SchoolContext")
-        {
-
-        }
-        public DbSet<Student> Students { get; set; }
-        public DbSet<Enrollment> Enrollments { get; set; }
         public DbSet<Course> Courses { get; set; }
-
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<OfficeAssignment> OfficeAssignments { get; set; }
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Entity<Course>()
+                .HasMany(c => c.Instructors).WithMany(i => i.Courses)
+            .Map(t => t.MapLeftKey("CourseID")
+            .MapRightKey("InstructorID")
+            .ToTable("CourseInstructor"));
         }
-
     }
 }
-
-//this file uses DbSet to set the tables with the corresponding info from the classes
