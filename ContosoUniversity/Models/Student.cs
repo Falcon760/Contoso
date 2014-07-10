0520dbf7a74845111e8231678a60bfa44f7f4a27
Falcon760 <sfalcon266@hotmail.com>
2014-07-10 15:49:58 -0400

    NEw

--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
+
 using System.Collections.Generic;
 
 
@@ -12,28 +11,16 @@ namespace ContosoUniversity.Models
     {
 
         public int ID { get; set; }
-        [Required]
-        [StringLength(50, MinimumLength=1)]
-        [Display(Name="Last Name")]
+
         public string LastName { get; set; }
-        [Required]
-        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
-        [Column("FirstName")]
-        [Display(Name="First Name")]
+
+
+
         public string FirstMidName { get; set; }
-        [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:yyyy-mm-dd}", ApplyFormatInEditMode=true)]
-        [Display(Name="Enrollment Date")]
+
         public DateTime EnrollmentDate { get; set; }
 
-        [Display(Name="Full Name")]
-        public string FullName
-        {
-            get
-            {
-                return LastName + ", " + FirstMidName;
-            }
-        }
+
 
         public virtual ICollection<Enrollment> Enrollments { get; set; }
 
